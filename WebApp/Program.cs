088124a2b896235c46725e4d9fa8fc53ce30dd60
SanketNyaynite <sanket.nyaynite@gmail.com>
2026-01-20 17:56:54 -0600

    Use app.Run to create Terminal Middleware

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -12,15 +12,22 @@ app.Use(async (HttpContext context, RequestDelegate next) =>
 });
 
 //Middleware #2
-app.Use(async (HttpContext context, RequestDelegate next) =>
+//app.Use(async (HttpContext context, RequestDelegate next) =>
+//{
+//    await context.Response.WriteAsync("Middleware #2: Before calling next\n\r");
+
+//    //if I comment below line, the control will not go to next middleware and will shortcircuit here.
+//    await next(context);
+
+//    await context.Response.WriteAsync("Middleware #2: After calling next\n\r");
+//});
+
+//app.Run helps to create Terminal Middleware #2
+app.Run(async (HttpContext context) =>
 {
-    await context.Response.WriteAsync("Middleware #2: Before calling next\n\r");
-
-    //if I comment below line, the control will not go to next middleware and will shortcircuit here.
-    await next(context);
-
-    await context.Response.WriteAsync("Middleware #2: After calling next\n\r");
+    await context.Response.WriteAsync("Terminal Middleware #2: Handling request and not calling next\n\r");
 });
+
 
 //Middleware #3
 app.Use(async (HttpContext context, RequestDelegate next) =>
