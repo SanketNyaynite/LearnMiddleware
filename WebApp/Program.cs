b62373ae8e65e32b61133bebf7486671afc9f6bd
SanketNyaynite <sanket.nyaynite@gmail.com>
2026-01-20 18:10:16 -0600

    Use app.Map to create a branching Middleware.

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -11,22 +11,40 @@ app.Use(async (HttpContext context, RequestDelegate next) =>
     await context.Response.WriteAsync("Middleware #1: After calling next\n\r");
 });
 
+app.Map("/employees", (appBuilder) =>
+{
+    //Branch Middleware #5
+    appBuilder.Use(async (HttpContext context, RequestDelegate next) =>
+    {
+        await context.Response.WriteAsync("Middleware #5: Before calling next\n\r");
+        await next(context);
+        await context.Response.WriteAsync("Middleware #5: After calling next\n\r");
+    });
+
+    appBuilder.Use(async (HttpContext context, RequestDelegate next) =>
+    {
+        await context.Response.WriteAsync("Middleware #6: Before calling next\n\r");
+        await next(context);
+        await context.Response.WriteAsync("Middleware #6: After calling next\n\r");
+    });
+});
+
 //Middleware #2
-//app.Use(async (HttpContext context, RequestDelegate next) =>
-//{
-//    await context.Response.WriteAsync("Middleware #2: Before calling next\n\r");
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("Middleware #2: Before calling next\n\r");
 
-//    //if I comment below line, the control will not go to next middleware and will shortcircuit here.
-//    await next(context);
+    //if I comment below line, the control will not go to next middleware and will shortcircuit here.
+    await next(context);
 
-//    await context.Response.WriteAsync("Middleware #2: After calling next\n\r");
-//});
+    await context.Response.WriteAsync("Middleware #2: After calling next\n\r");
+});
 
 //app.Run helps to create Terminal Middleware #2
-app.Run(async (HttpContext context) =>
-{
-    await context.Response.WriteAsync("Terminal Middleware #2: Handling request and not calling next\n\r");
-});
+//app.Run(async (HttpContext context) =>
+//{
+//    await context.Response.WriteAsync("Terminal Middleware #2: Handling request and not calling next\n\r");
+//});
 
 
 //Middleware #3
@@ -46,5 +64,6 @@ app.Run();
 /* NOTES
  * Regular middleware and terminal(function) middleware. Middleware shortcircuiting.  
  * Use method to create middleware.
- * 
+ * app.Run is used to create terminal middleware.
+ * app.Map to create branching middleware.
  */
